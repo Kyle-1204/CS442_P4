a52ccb8110f400679a20773afc8615b896f22cc7
Kyle Enriquez <kenriqu1@binghamton.edu>
2023-11-26 01:05:08 -0500

    Added initial files and completed remote classes

---
+++ assignment04/FanRemote.java
@@ -0,0 +1,30 @@
+package assignment04;
+
+public class FanRemote {
+	private Fan fan;
+	public FanRemote(Fan fanIn) {
+		fan = fanIn;
+		// TODO
+		// make calls to fan.slower() to set the fan speed to 0; notice that
+		// the test driver sets the fan to maximum before connecting the remote.
+		while (fan.getSpeed() != 0){
+			fan.slower();
+		}
+	}
+
+	public void setSpeed(int newSpeed) {
+		// TODO
+		// write this method using multiple calls to fan.faster() or
+		// fan.slower() as appropriate
+		while (fan.getSpeed() != newSpeed){
+			if (fan.getSpeed() < newSpeed){
+				fan.faster();
+			}
+			if (fan.getSpeed() > newSpeed){
+				fan.slower();
+			}
+		}
+	}
+
+}
+
---
+++ assignment04/ThermostatRemote.java
@@ -0,0 +1,30 @@
+package assignment04;
+
+public class ThermostatRemote {
+	private Thermostat stat;
+	public ThermostatRemote(Thermostat statIn) {
+		stat = statIn;
+		// TODO
+		// make calls to stat.cooler() to set the temperature to the minimum
+		// notice the test driver raises the temp before connecting the remote.
+		while (stat.getTemperature() != 50){
+			stat.cooler();
+		}
+	}
+
+	public void setTemperature(int newTemp){
+		// TODO
+		// write this method using multiple calls to stat.warmer() or
+		// stat.cooler() as appropriate
+		while (stat.getTemperature() != newTemp){
+			if (stat.getTemperature() < newTemp){
+				stat.warmer();
+			}
+			if (stat.getTemperature() > newTemp){
+				stat.cooler();
+			}
+		}
+
+	}
+}
+
